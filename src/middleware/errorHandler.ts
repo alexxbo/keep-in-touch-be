3f16672e5459125a3e87396d46377b7c4bf70007
a.borovskoy <aborovskoy@forwardslashsoftware.com>
2025-08-03 21:10:57 +0300

    feat: improve error handling in middleware; add Zod validation error handling and adjust logging

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,5 +1,6 @@
 import {NextFunction, Request, Response} from 'express';
 import {StatusCodes} from 'http-status-codes';
+import z from 'zod';
 import {BaseError} from '../utils/BaseError';
 import {logger} from '../utils/logger';
 
@@ -10,19 +11,11 @@ const errorHandler = (
   next: NextFunction, // eslint-disable-line @typescript-eslint/no-unused-vars
 ) => {
   const isProduction = process.env.NODE_ENV === 'production';
-  const needToLogStack = !isProduction && err.statusCode === undefined;
+  let needToLogStack = false;
 
   if (!(err instanceof Error)) {
     err = new Error(err);
   }
-
-  // Log the error
-  logger.error(
-    `${err.statusCode ? `statusCode: ${err.statusCode}` : ''} message: ${err.message}`,
-    {
-      stack: needToLogStack ? err.stack : undefined,
-    },
-  );
 
   err.statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   err.message = err.message || 'Internal server error';
@@ -51,12 +44,27 @@ const errorHandler = (
     err = new BaseError(message, StatusCodes.UNAUTHORIZED);
   }
 
+  // Zod validation error
+  if (err instanceof z.ZodError) {
+    err = new BaseError(z.prettifyError(err), StatusCodes.BAD_REQUEST);
+  }
+
   // Operational errors should not leak details in production
   if (!(err instanceof BaseError) && isProduction) {
     err.message = 'An unexpected error occurred. Please try again later.';
+    needToLogStack = !isProduction && err.statusCode === undefined;
   }
 
+  // Log the error
+  logger.error(
+    `${err.statusCode ? `statusCode: ${err.statusCode}` : ''} message: ${err.message}`,
+    {
+      stack: needToLogStack ? err.stack : undefined,
+    },
+  );
+
   res.status(err.statusCode).json({
+    success: false,
     status: err.statusCode,
     message: err.message,
     stack: needToLogStack ? err.stack : undefined,
